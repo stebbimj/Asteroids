ca39cecedaeadfff522f5690c2edea9b5aaaca33
stebbimj <stebbimj@live.com>
2025-05-10 22:07:43 +0200

    Display connected and infinite loop created

--- main.py
+++ main.py
@@ -3,8 +3,18 @@ import constants
 
 def main():
     print("Starting Asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    pygame.init()
+    #print(f"Screen width: {constants.SCREEN_WIDTH}")
+    #print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    while True:    
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+    # Draw a black square inside the game area and redraw it as long as the program runs
+        screen.fill((0,0,0))
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
