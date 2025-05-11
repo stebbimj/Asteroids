4071b983a7576ed2bdad1575158cabf4c3fd52dd
stebbimj <stebbimj@live.com>
2025-05-11 06:57:40 +0200

    Circleshape and player created, player drawn to screen

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import pygame
-import constants
+from constants import *
+from circleshape import *
+from player import *
 
 def main():
     print("Starting Asteroids!")
     pygame.init()
     clock= pygame.time.Clock()
     dt = 0
+    player1 = player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     #print(f"Screen width: {constants.SCREEN_WIDTH}")
     #print(f"Screen height: {constants.SCREEN_HEIGHT}")
-    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:    
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -16,10 +19,12 @@ def main():
 
     # Draw a black square inside the game area and redraw it as long as the program runs
         screen.fill((0,0,0))
+        player1.draw(screen)
+
         pygame.display.flip()
+
         dt = clock.tick(60)
         dt /= 1000
-        print(dt)
 
 
 if __name__ == "__main__":
