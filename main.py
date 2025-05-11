0a298aa6341b047737cdb87ea5413ee8fc20b36d
stebbimj <stebbimj@live.com>
2025-05-11 06:22:53 +0200

    clock and FPS generator added

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import constants
 def main():
     print("Starting Asteroids!")
     pygame.init()
+    clock= pygame.time.Clock()
+    dt = 0
     #print(f"Screen width: {constants.SCREEN_WIDTH}")
     #print(f"Screen height: {constants.SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
@@ -15,6 +17,10 @@ def main():
     # Draw a black square inside the game area and redraw it as long as the program runs
         screen.fill((0,0,0))
         pygame.display.flip()
+        dt = clock.tick(60)
+        dt /= 1000
+        print(dt)
+
 
 if __name__ == "__main__":
     main()
